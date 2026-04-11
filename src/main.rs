002607e49b4d372b02da45e5148bbc3c46e8dbf6
wiz <guerand.julien77@gmail.com>
2026-04-11 17:43:13 +0200

    added a panic visual effect

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,23 @@ use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    let all_char_bytes: &[u8; 29] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#";
+    {
+        for byte in all_char_bytes {
+            let mut writer = crate::vga_buffer::WRITER.lock();
+
+            for _ in 0..5 {
+                writer.fill_with_char(*byte);
+            }
+            
+            drop(writer); 
+
+            for _ in 0..50_000 {
+                core::hint::spin_loop();
+            }
+        }
+    }
+    crate::vga_buffer::WRITER.lock().clear_screen();
     println!("[PANIC] {}", _info);
     loop {}
 }
